b5ddc3dbd3dd1658f7a456b854cd35a3f0bba423
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-05-30 02:12:11 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import jobListingsData from './data.json';
 import './App.css';
 import JobListing from './JobListing';
+import { ReactComponent as Header } from './bg-header-desktop.svg'
 
 function App() {
    
   return (
     <div className="App">
+      <Header className='header'/>
       {jobListingsData.map ((jobListing) => 
         < JobListing  jobListing= {jobListing} /> 
       )}
