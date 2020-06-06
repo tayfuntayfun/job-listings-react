b57f0d3852ffe3171008f01556519779ac04f374
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-06-06 01:07:25 +0100

    update

--- src/Favorites.js
+++ src/Favorites.js
@@ -4,12 +4,25 @@ import React from 'react'
 const Favorites = (props) => {
  
     return(
-      <div className={props.jobFilter.length >0 ? "favorites-Container" : 'no-show'} >
+      <div className={props.jobFilter.length >0 ? "favorites-Container" : null } >
          {props.jobFilter
          .map((element) => {
          return(
-         <div className={'language-buttons'}
-              onClick={() => props.removeFilter(element)}>{element}</div>)
+           <div className='tags-and-close' 
+              style={{
+              display: 'flex',
+              flexDirection:'row'
+         }}>
+              <div className={'language-buttons'}
+              >{element}</div>
+              <i style={{padding:'5px', 
+                        background:'red',
+                        borderRadius:'3px',
+                        
+                      }}
+              onClick={() => props.removeFilter(element)}><b>X</b></i>
+           </div>)
+         
          })}
      </div>
    );
